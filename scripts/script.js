88f2a318e4794a1dfd1c3a4f0e7aaebe41a067fd
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-10-24 21:42:59 +0100

    Update

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+function toggleSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    const mainContainer = document.querySelector('.main-container');
+    const nameSite = document.querySelector('.site-name');
+    const imgLogo = document.querySelector('.logo-img');
+
+    sidebar.classList.toggle('closed');
+
+    if (sidebar.classList.contains('closed')) {
+        mainContainer.style.marginLeft = '110px';
+        nameSite.style.display = 'none';
+        imgLogo.style.width = '65px';
+        imgLogo.style.height = '65px';
+    } else {
+        mainContainer.style.marginLeft = '270px';
+        nameSite.style.display = 'flex';
+        imgLogo.style.width = '80px';
+        imgLogo.style.height = '80px';
+    }
+}
+
+const menuIcon = document.querySelector('.menu-icon');
+menuIcon.addEventListener('click', toggleSideBar);
